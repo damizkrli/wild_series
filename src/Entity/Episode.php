419fc01267fb11023d5f9c151572b037211f2fda
Damien <karli.damien@gmail.com>
2020-06-03 21:15:22 +0200

    add CSS + modification of secondary color + Synfony_13 begin

--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\EpisodeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass=EpisodeRepository::class)
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,6 +6,8 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
@@ -21,6 +23,9 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Blank(message="The title cannot be empty")
+     * @Assert\Length(max = "255", maxMessage="The title is too long and must not exceed {{ max }} characters")
+     *
      */
     private $title;
 
