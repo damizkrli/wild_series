ed1d0d867b57d41f3138f6767ebd20cd0c4efb7c
Damien <karli.damien@gmail.com>
2020-06-22 11:28:29 +0200

    create Comment Entity, create realtion between comment and Episode and User, Create Comment Crud

---
+++ src/Entity/Comment.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Episode::class)
+     */
+    private $episodes;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $users;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getRate(): ?int
+    {
+        return $this->rate;
+    }
+
+    public function setRate(int $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getEpisodes(): ?Episode
+    {
+        return $this->episodes;
+    }
+
+    public function setEpisodes(?Episode $episodes): self
+    {
+        $this->episodes = $episodes;
+
+        return $this;
+    }
+
+    public function getUsers(): ?User
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?User $users): self
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+}
