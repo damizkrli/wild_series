9db40a63bae0a0f2dd63247f66e38ede0189f4c6
Damien <karli.damien@gmail.com>
2020-06-22 16:31:15 +0200

    Entity Comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -27,29 +27,6 @@ class Comment
      */
     private $rate;
 
-    /**
-<<<<<<< HEAD
-     * @ORM\ManyToOne(targetEntity=Episode::class)
-     */
-    private $episodes;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class)
-     */
-    private $users;
-=======
-     * @ORM\ManyToOne(targetEntity=User::class)
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $user;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Episode::class)
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $episodes;
->>>>>>> 494c0bc90a6eef499283e38882ca237333bb6195
-
     public function getId(): ?int
     {
         return $this->id;
@@ -75,52 +52,6 @@ class Comment
     public function setRate(int $rate): self
     {
         $this->rate = $rate;
-
-        return $this;
-    }
-
-<<<<<<< HEAD
-    public function getEpisodes(): ?Episode
-    {
-        return $this->episodes;
-    }
-
-    public function setEpisodes(?Episode $episodes): self
-    {
-        $this->episodes = $episodes;
-=======
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
->>>>>>> 494c0bc90a6eef499283e38882ca237333bb6195
-
-        return $this;
-    }
-
-<<<<<<< HEAD
-    public function getUsers(): ?User
-    {
-        return $this->users;
-    }
-
-    public function setUsers(?User $users): self
-    {
-        $this->users = $users;
-=======
-    public function getEpisodes(): ?Episode
-    {
-        return $this->episodes;
-    }
-
-    public function setEpisodes(?Episode $episodes): self
-    {
-        $this->episodes = $episodes;
->>>>>>> 494c0bc90a6eef499283e38882ca237333bb6195
 
         return $this;
     }
